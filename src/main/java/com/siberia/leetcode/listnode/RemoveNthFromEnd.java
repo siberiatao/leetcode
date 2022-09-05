5a1126dd32df3602ef8bb20592dc4334f4b0ab94
SiberiaTao <siberiatao@icloud.com>
2022-09-06 00:06:14 +0800

    更新双指针写法

--- src/main/java/com/siberia/leetcode/listnode/RemoveNthFromEnd.java
+++ src/main/java/com/siberia/leetcode/listnode/RemoveNthFromEnd.java
@@ -47,4 +47,22 @@ public class RemoveNthFromEnd {
         pre.next = cur.next;
         return head;
     }
+
+    /**
+     * 双指针写法，走天下
+     */
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
 }
